a13884d44fc13f5f76b533a376cc459cc8df225f
204Srihari <sriharivenkatesh24@gmail.com>
2025-07-04 14:37:12 +0530

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -2,13 +2,7 @@
 
 // Firebase v10 (compat) – browser-friendly
 const firebaseConfig = {
-    apiKey: "AIzaSyBqh77-Izs_IKKTRCbUx5JJABcRQpZEoCk",
-    authDomain: "webdev-59cc2.firebaseapp.com",
-    projectId: "webdev-59cc2",
-    storageBucket: "webdev-59cc2.appspot.com",
-    messagingSenderId: "252485754460",
-    appId: "1:252485754460:web:66ec2c905c978c7f7ed099",
-    measurementId: "G-3RJDBQX73Q"
+//add u r app configuration
   };
   
   // Initialize Firebase App
